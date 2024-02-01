e1a4fc7b8260f0ee6e01735439703bb3222e8d1e
Francisco Nieto <vivertido@gmail.com>
2024-01-31 21:49:58 -0500

    added links from index

--- flask-pg/app.py
+++ flask-pg/app.py
@@ -43,4 +43,4 @@ def info():
     return render_template('info.html', temp=temp, ipaddr=ipaddr, freemem=freemem, cpuinfo=cpuinfo, uptime=uptime)
 
 if __name__ == '__main__':
-    app.run( debug=True, host='0.0.0.0')
+    app.run( debug=True, host='0.0.0.0', port=2022)
